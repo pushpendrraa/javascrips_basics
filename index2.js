05bfeb0804feb1d8a9938e5da2414faa3cf9ea3a
Pushpendra <pushpendra91104@gmail.com>
2026-01-16 02:11:10 +0530

    practice

---
+++ index2.js
@@ -0,0 +1,36 @@
+let arr=[2,4,13,55,202,1,11];
+let comp=(a,b)=>{
+    return a-b;
+}
+arr.sort(comp);
+console.log(arr);
+let arr=[8,643,,68,652,6,8,87,3,3,6,7,];
+console.log(arr.length)
+
+arr.forEach((el)=>{
+    
+console.log(el*el);
+})
+let af=arr.filter((val)=>{
+    console.log(val);
+return val<10;
+}
+)
+console.log(a);
+
+console.time("tm");
+let a=0;
+while(a<100){
+    console.log(a*a);
+    a++;
+}
+console.timeEnd("tm");
+function f(){
+    console.log("My pet");
+}
+
+setTimeout(function (){
+    console.log("hii"),3000
+})
+console.log("byyy");
+console.log("again byyy")
