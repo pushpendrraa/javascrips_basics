f1e6186a2a2956d3f3f928c9ac1dee9170bd5724
Pushpendra <pushpendra91104@gmail.com>
2026-01-19 07:00:57 +0530

    closure conceps

---
+++ closures.js
@@ -0,0 +1,30 @@
+function counter(){
+    let count=0;  //lives in the lexical environment of increment & getcount
+   function increment(){
+        count++;
+        console.log(`count increase to ${count}`)
+    }
+   function getcount(){
+    return count;
+   }
+   return{increment,getcount};
+}
+const counte =counter();
+counte.increment();
+counte.increment();
+counte.increment();  //In this example, the local variable count is not destroyed
+                    //  because it is captured by inner functions, 
+                    // forming a closure that keeps the lexical environment alive.
+console.log(`the current count is ${counte.getcount()}`)
+
+function test() {
+    let count = 0;
+    count++;
+    console.log(count);
+}
+
+test(); // 1
+test(); // 1
+test(); // 1
+//in this example local variales are destroyed beacuse there is not any reference for these variables
+//so js see after execution of this function no need of local variablrs
