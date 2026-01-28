d21783bbaa5e6964d12b91d858089c3038570ca7
Pushpendra <pushpendra91104@gmail.com>
2026-01-29 01:31:36 +0530

    basic colour changing program

---
+++ DOM.js
@@ -0,0 +1,10 @@
+const box=document.getElementById("Box");
+
+function doyellow(){
+    box.style.backgroundColor="yellow";
+};
+function dored(){
+    box.style.backgroundColor="red";
+};
+
+//this is a basic colour changing program when user clicks on a button
