7b61221940b7c5a9972199effb96f8c675f2d2b8
Pushpendra <pushpendra91104@gmail.com>
2026-02-28 01:51:38 +0530

    then and catch

--- promises.js
+++ promises.js
@@ -1,7 +1,22 @@
-let promise=new Promise((reject,resolve)=>{
- setTimeout(()=>{
-    console.log("hello");
- },10000);
- resolve(1);
-}
-);
+// let promise=new Promise((reject,resolve)=>{
+//  setTimeout(()=>{
+//     console.log("hello");
+//  },10000);
+//  resolve(1);
+// }
+// );
+
+let promise=new Promise((resolve,reject)=>{
+    let success=false;
+    if(success){
+        resolve("Hiiiiiiii");
+    }
+    else{
+        reject("fails");
+    }
+});
+promise.then((msg)=>{
+    console.log("promise message "+ msg);
+}).catch((error)=>{
+    console.log(error);
+})
